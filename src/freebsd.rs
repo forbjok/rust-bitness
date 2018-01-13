e1a1cb966ffbdfc5f5f9951879bbc4bbd264b21e
Forb.Jok <forbjok@gmail.com>
2018-01-13 01:12:01 +0100

    feat: Support FreeBSD

---
+++ src/freebsd.rs
@@ -0,0 +1,33 @@
+use sysctl;
+
+use super::*;
+
+/// Get the OS's bitness independently of the executable's bitness.
+///
+/// This means if the user is running a 64-bit OS, os_bitness()
+/// will return Bitness::X86_64 regardless of whether the executable is compiled
+/// for x86 or x86-64.
+///
+/// # Examples
+/// ```
+/// let bitness = bitness::os_bitness();
+/// ```
+pub fn os_bitness() -> Bitness {
+    /* Use kern.supported_archs, as hw.machine only returns the architecture of the executable. */
+    let supported_archs = sysctl::value("kern.supported_archs").unwrap();
+
+    if let sysctl::CtlValue::String(supported_archs) = supported_archs {
+        if supported_archs.split(" ").any(|m| m == "amd64") {
+            Bitness::X86_64
+        }
+        else if supported_archs.split(" ").any(|m| m == "i386") {
+            Bitness::X86_32
+        }
+        else {
+            Bitness::Unknown
+        }
+    }
+    else {
+        Bitness::Unknown
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,23 @@
 #[cfg(windows)]
 extern crate winapi;
-#[cfg(unix)]
+#[cfg(all(unix, not(target_os = "freebsd")))]
 extern crate uname;
+#[cfg(target_os = "freebsd")]
+extern crate sysctl;
 
 #[cfg(windows)]
 mod windows;
-#[cfg(unix)]
+#[cfg(all(unix, not(target_os = "freebsd")))]
 mod unix;
+#[cfg(target_os = "freebsd")]
+mod freebsd;
 
 #[cfg(windows)]
 pub use windows::*;
-#[cfg(unix)]
+#[cfg(all(unix, not(target_os = "freebsd")))]
 pub use unix::*;
+#[cfg(target_os = "freebsd")]
+pub use freebsd::*;
 
 #[derive(Debug, PartialEq)]
 pub enum Bitness {
